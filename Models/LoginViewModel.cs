93e366d0a76c6f366b7aa757cfcd8b6da05f7768
Ignacio Lopez <ignacio.ujat@gmail.com>
2023-05-18 13:59:21 -0600

    Funcionalidad de login agregado

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TareasMVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo debe de ser Un Correo Electronico valido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Recuerdame")]
+        public bool RememberMe { get; set; }
+    }
+}
