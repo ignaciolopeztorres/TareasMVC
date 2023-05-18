d4bf97e078eb95536bddb6bc0994d3a1937867db
ignacio <nacho@localhost.com>
2023-05-18 00:36:35 -0600

    Genera la configuracion de entity framework, como servicio y con su cadena de connecion a la base de datos

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TareasMVC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
