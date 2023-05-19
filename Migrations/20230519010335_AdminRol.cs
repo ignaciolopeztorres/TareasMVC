42b2cf2194f63ace4670480f8edf31ebdda4b2a6
Ignacio Lopez <ignacio.ujat@gmail.com>
2023-05-18 19:15:49 -0600

    Creando rol de admin usando migraciones

---
+++ Migrations/20230519010335_AdminRol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT id FROM AspNetRoles WHERE Id = 'a9e9e697-d960-498f-b1a6-f2d89d5372b4')
+                BEGIN
+	                INSERT AspNetRoles (Id, [Name], [NormalizedName])
+	                VALUES ('a9e9e697-d960-498f-b1a6-f2d89d5372b4', 'admin', 'ADMIN')
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles WHERE Id = 'a9e9e697-d960-498f-b1a6-f2d89d5372b4'");
+        }
+    }
+}
