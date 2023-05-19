923bc7b6e5b9d57ae1cf41047d4847a294cdd772
Ignacio Lopez <ignacio.ujat@gmail.com>
2023-05-19 11:23:02 -0600

    Agregando nuevsa tareas en memoria

---
+++ wwwroot/js/Tareas.js
@@ -0,0 +1,14 @@
+﻿function agregarNuevaTareaAlListado() {
+    tareaListadoViewModel.tareas.push(new tareaElementoListadoViewModel({ id: 0, titulo: '' }));
+    $("[name=titulo-tarea]").last().focus();
+}
+
+function manejarFocusoutTituloTarea(tarea) {
+    const titulo = tarea.titulo();
+    if (!titulo) {
+        tareaListadoViewModel.tareas.pop();
+        return;
+    }
+
+    tarea.id(1);
+}
