273c6cfaab92a9437ec80b50826bedacce1c9e2e
Ignacio Lopez <ignacio.ujat@gmail.com>
2023-05-19 21:35:55 -0600

    UI para insertar los pasos de la tarea

---
+++ wwwroot/js/Pasos.js
@@ -0,0 +1,10 @@
+﻿function manejarClickAgregarPaso() {
+    const indice = tareaEditarVM.pasos().findIndex(p => p.esNuevo());
+
+    if (indice !== -1) {
+        return;
+    }
+
+    tareaEditarVM.pasos.push(new pasoViewModel({ modoEdicion: true, realizado: false }));
+    $("[name=txtPasoDescripcion]:visible").focus();
+}
