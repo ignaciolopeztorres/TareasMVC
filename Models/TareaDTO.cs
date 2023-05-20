c130f33cd1a4ae70fa2a28bbf6fceeb0ccb89fa7
Ignacio Lopez <ignacio.ujat@gmail.com>
2023-05-20 11:58:05 -0600

    Obteniendo  Datos de conteo con EF Core

--- Models/TareaDTO.cs
+++ Models/TareaDTO.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
+        public string PasosRealizados { get; set; }
+        public string PasosTotal { get; set; }
     }
 }
