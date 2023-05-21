ad3e698e45db711facbd94c2de34c4a6efd034cd
Ignacio Lopez <ignacio.ujat@gmail.com>
2023-05-20 21:21:18 -0600

    Insertando archivos en la base de datos

---
+++ Servicios/AlmacenarArchivoResultado.cs
@@ -0,0 +1,8 @@
+﻿namespace TareasMVC.Servicios
+{
+    public class AlmacenarArchivoResultado
+    {
+        public string URL { get; set; }
+        public string Titulo { get; set; }
+    }
+}
---
+++ Servicios/IAlmacenadorArchivos.cs
@@ -0,0 +1,9 @@
+﻿namespace TareasMVC.Servicios
+{
+    public interface IAlmacenadorArchivos
+    {
+        Task Borrar(string ruta, string contenedor);
+
+        Task<AlmacenarArchivoResultado[]> Almacenar(string contenedor, IEnumerable<IFormFile> archivos);
+    }
+}
